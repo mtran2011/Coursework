36eac4379f35ac52aaea11bc8f3402790e203d6b
Minh Ha Tran <tranhaminh@gmail.com>
2017-05-07 19:35:29 -0400

    update

---
+++ sabr_helper_functions.py
@@ -0,0 +1,59 @@
+from __future__ import division
+import numpy as np
+import scipy.stats as ss
+from math import log, exp, sqrt
+
+def black_scholes(S0, K, r, q, sigma, T, option_type):
+    d1 = (log(S0 / K) + (r - q + sigma**2 / 2) * T) / (sigma * sqrt(T))
+    d2 = d1 - sigma * sqrt(T)
+    if option_type == 'call':
+        return exp(-q * T) * S0 * ss.norm.cdf(d1) - K * exp(-r * T) * ss.norm.cdf(d2)        
+    elif option_type == 'put':
+        return - exp(-q * T) * S0 * ss.norm.cdf(-d1) + K * exp(-r * T) * ss.norm.cdf(-d2)
+    else:
+        raise ValueError('input either "call" or "put" or option_type')    
+    
+def call_delta(S0, r, q, T, sigma, K):
+    d1 = (log(S0 / K) + (r - q + sigma**2 / 2) * T) / (sigma * sqrt(T))
+    return ss.norm.cdf(d1) * exp(-q * T)
+
+def put_delta(S0, r, q, T, sigma, K):
+    d1 = (log(S0 / K) + (r - q + sigma**2 / 2) * T) / (sigma * sqrt(T))
+    return -ss.norm.cdf(-d1) * exp(-q * T)
+
+def call_strike(delta, sigma, r, q, S0, T):
+    '''
+    Calculate the strike so that the delta of a call is same as target, for a given sigma
+    '''
+    if delta < 0:
+        delta = 0
+    if delta > 1:
+        delta = 1
+    d1 = ss.norm.ppf(delta / exp(-q * T))
+    return S0 / exp(d1 * (sigma * sqrt(T)) - (r - q + 0.5 * sigma**2) * T)
+
+def put_strike(delta, sigma, r, q, S0, T):
+    '''
+    Calculate the strike so that the delta of a put is same as target, for a given sigma
+    '''
+    if delta < -1:
+        delta = -1
+    if delta > 0:
+        delta = 0
+    d1 = - ss.norm.ppf(-delta / exp(-q * T))
+    return S0 / exp(d1 * (sigma * sqrt(T)) - (r - q + 0.5 * sigma**2) * T)
+
+def atm_dns_K(S0, r, q, T, sigma):
+    '''
+    Calculate the ATM strike for a delta neutral straddle
+    '''
+    return S0 * exp((r - q + 0.5 * sigma**2) * T)
+    
+def find_sabr_vol(K, F, T, alpha, beta, nu, rho=0):
+    '''
+    Given strike and expiry, find SABR implied vol     
+    '''
+    z = (nu/alpha) * (F*K)**((1-beta)/2) * log(F/K) 
+    x = log(((1 - 2*rho*z + z**2)**0.5 + z - rho) / (1 - rho))
+    factor = (1 + ((1-beta)**2 / 24 * alpha**2 / (F*K)**(1-beta) + 0.25 * (rho*beta*nu*alpha) / (F*K)**((1-beta)/2) + (2 - 3 * rho**2) / 24 * nu**2) * T)
+    return alpha / ((F*K)**((1-beta)/2) * (1 + ((1-beta)**2)/24 * log(F/K)**2 + ((1-beta)**4)/1920 * log(F/K)**4)) * (z/x) * factor
--- test_sabr.py
+++ test_sabr.py
@@ -29,15 +29,15 @@ class SabrTestCase(unittest.TestCase):
         F = S0 * exp((r - q) * T)
         
         # first condition 
-        guess_K_atm = find_K_dns_atm(S0, r, q, T, sigma_atm)        
+        guess_K_atm = atm_dns_K(S0, r, q, T, sigma_atm)        
         delta_atm_call = call_delta(S0, r, q, T, sigma_atm, guess_K_atm)
         delta_atm_put = put_delta(S0, r, q, T, sigma_atm, guess_K_atm)
         self.assertAlmostEqual(delta_atm_call + delta_atm_put, 0, 6)
         self.assertAlmostEqual(guess_K_atm, K_atm, 2)
         
         # second condition        
-        sabr_vol_bf_call = sabr_implied_vol(K_bf_call, F, T, alpha, beta, nu)
-        sabr_vol_bf_put = sabr_implied_vol(K_bf_put, F, T, alpha, beta, nu)
+        sabr_vol_bf_call = find_sabr_vol(K_bf_call, F, T, alpha, beta, nu)
+        sabr_vol_bf_put = find_sabr_vol(K_bf_put, F, T, alpha, beta, nu)
         call_bf_sabr = black_scholes(S0, K_bf_call, r, q, sabr_vol_bf_call, T, 'call')
         put_bf_sabr = black_scholes(S0, K_bf_put, r, q, sabr_vol_bf_put, T, 'put')        
         call_bf_bs = black_scholes(S0, K_bf_call, r, q, sigma_atm + sigma_bf, T, 'call')
@@ -45,8 +45,8 @@ class SabrTestCase(unittest.TestCase):
         self.assertAlmostEqual(call_bf_bs + put_bf_bs, call_bf_sabr + put_bf_sabr, 6)
         
         # third condition
-        sabr_vol_rr_call = sabr_implied_vol(K_rr_call, F, T, alpha, beta, nu)
-        sabr_vol_rr_put = sabr_implied_vol(K_rr_put, F, T, alpha, beta, nu)
+        sabr_vol_rr_call = find_sabr_vol(K_rr_call, F, T, alpha, beta, nu)
+        sabr_vol_rr_put = find_sabr_vol(K_rr_put, F, T, alpha, beta, nu)
         self.assertAlmostEqual(sabr_vol_rr_call, vol_rr_call, 5)
         self.assertAlmostEqual(sabr_vol_rr_put, vol_rr_put, 5)
         self.assertAlmostEqual(sabr_vol_rr_call - sabr_vol_rr_put, sigma_rr, 4)
