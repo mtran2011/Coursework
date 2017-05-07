779e23ed04dadc1f52d6d346f764be7bf0e791a9
Minh Ha Tran <tranhaminh@gmail.com>
2017-05-07 01:00:34 -0400

    update

--- test_sabr.py
+++ test_sabr.py
@@ -15,7 +15,7 @@ class SabrTestCase(unittest.TestCase):
         beta = 1.0667
         nu = 1.26488        
         
-        K_atm = 1.09944 
+        K_atm = 1.10087 
         K_bf_put = 1.07093 
         K_bf_call = 1.13586
         K_rr_put = 1.071 
@@ -26,21 +26,22 @@ class SabrTestCase(unittest.TestCase):
         vol_rr_put = 0.105501
         vol_rr_call = 0.106001
         
-        F = S0 * exp((r - q) * T)        
+        F = S0 * exp((r - q) * T)
+        
         # first condition 
         guess_K_atm = find_K_dns_atm(S0, r, q, T, sigma_atm)        
         delta_atm_call = call_delta(S0, r, q, T, sigma_atm, guess_K_atm)
         delta_atm_put = put_delta(S0, r, q, T, sigma_atm, guess_K_atm)
         self.assertAlmostEqual(delta_atm_call + delta_atm_put, 0, 6)
         self.assertAlmostEqual(guess_K_atm, K_atm, 2)
-                
+        
+        # second condition        
         sabr_vol_bf_call = sabr_implied_vol(K_bf_call, F, T, alpha, beta, nu)
         sabr_vol_bf_put = sabr_implied_vol(K_bf_put, F, T, alpha, beta, nu)
         call_bf_sabr = black_scholes(S0, K_bf_call, r, q, sabr_vol_bf_call, T, 'call')
         put_bf_sabr = black_scholes(S0, K_bf_put, r, q, sabr_vol_bf_put, T, 'put')        
         call_bf_bs = black_scholes(S0, K_bf_call, r, q, sigma_atm + sigma_bf, T, 'call')
-        put_bf_bs = black_scholes(S0, K_bf_put, r, q, sigma_atm + sigma_bf, T, 'put')
-        # second condition 
+        put_bf_bs = black_scholes(S0, K_bf_put, r, q, sigma_atm + sigma_bf, T, 'put')         
         self.assertAlmostEqual(call_bf_bs + put_bf_bs, call_bf_sabr + put_bf_sabr, 6)
         
         # third condition
