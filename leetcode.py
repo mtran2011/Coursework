8c9a4f99dc0f566f52b7041f3c70119506991e8a
mtran2011 <tranhaminh@gmail.com>
2017-12-14 23:51:39 -0500

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -162,6 +162,94 @@ class Solution:
                 current.right.level = current.level + 1
                 q.enqueue(current.level)
 
+# 94. Binary Tree Inorder Traversal
+class Solution:
+    def inorderTraversal(self, root):
+        res = []
+        from queue import LifoQueue
+        stack = LifoQueue()
+        current = root
+        while current is not None:
+            stack.put(current)
+            current = current.left
+        # now that current is none
+        x = stack.get()
+        res.append(x)
+        current = x.right
+        # now go back to the above while loop, to do this see below
+class Solution:
+    def inorderTraversal(self, root):
+        res = []
+        from queue import LifoQueue
+        stack = LifoQueue()
+        current = root
+        while current is not None or not stack.empty():
+            while current is not None:
+                stack.put(current)
+                current = current.left
+            # now that current has become none
+            x = stack.get()
+            res.append(x.val)
+            current = x.right
+        return res
+
+# 62. Unique Paths
+class Solution:
+    def uniquePaths(self, m, n):
+        f = [[1 for j in range(n)] for i in range(m)]
+        for i in range(m-2,-1,-1):
+            for j in range(n-2,-1,-1):
+                f[i][j] = f[i+1][j] + f[i][j+1]
+        return f[0][0]
+
+# 55. Jump Game
+# Given an array of non-negative integers, you are initially positioned at the first index of the array.
+# Each element in the array represents your maximum jump length at that position.
+# Determine if you are able to reach the last index.
+# A = [2,3,1,1,4], return true.
+# A = [3,2,1,0,4], return false.
+class Solution:
+    def canJump(self, nums):
+        n = len(nums)
+        if n < 1:
+            return True
+        f = [False for _ in range(n)]
+        f[-1] = True
+        for i in range(n-2,-1,-1):
+            # try to determine f[i] here
+            # the max number of steps you can take from here is either a[i] or n-1-i
+            max_steps = min(nums[i], n-1-i)
+            for step_size in range(1,max_steps+1):
+                if f[i + step_size] is True:
+                    f[i] = True
+        # this is not O(N)
+        return f[0]
+# the above is O(N**2); the key to O(N) is if you can find an index j where reachable is true, 
+# then reaching n is equivalent to reaching j as reaching j takes fewer steps so if smt can reach n, definitely can reach j
+class Solution:
+    def canJump(self, nums):
+        n = len(nums)
+        if n < 1:
+            return True
+        f = [False for _ in range(n)]
+        f[-1] = True
+        smallest_starting_position = n-1
+        for i in range(n-2,-1,-1):
+            if smallest_starting_position - i <= nums[i]:
+                f[i] = True
+                smallest_starting_position = i
+        return f[0]
+class Solution:
+    def canJump(self, nums):
+        n = len(nums)
+        if n < 1:
+            return True
+        smallest_starting_position = n-1
+        for i in range(n-2,-1,-1):
+            if smallest_starting_position - i <= nums[i]:                
+                smallest_starting_position = i
+        return smallest_starting_position == 0
+
 # 98. Validate Binary Search Tree
 class Solution:
     def traversal(self, root):
@@ -172,19 +260,31 @@ class Solution:
         if root.left is None and root.right is None:
             return True, root.val, root.val
         
+        # now that root has at least 1 child
         # left_min should be min of this whole tree starting at root 
-        # also need to check left_max <= root.val
-        if root.left is not None:
-            left_is_bst, left_min, left_max = traversal(root.left)
+        # also need to check left_max < root.val
+        if root.left is None:
+            left_is_bst = True
+            left_min = root.val
+            left_is_smaller = True
+            # in this case root.right is not none
+            right_is_bst, right_min, right_max = self.traversal(root.right)
+            right_is_bigger = root.val < right_min
+        elif root.right is None:
+            right_is_bst = True
+            right_max = root.val
+            right_is_bigger = True
+            # here root.left is not none            
+            left_is_bst, left_min, left_max = self.traversal(root.left)
+            left_is_smaller = left_max < root.val
         else:
-            left_is_bst, left_min, left_max = True, root.val, root.val            
-        
-        # right_max should be max of this whole tree starting at root
-        # need to check if root.val <= right_min
-        if root.right is not None:
-            right_is_bst, right_min, right_max = traversal(root.right)
-        else:
-            right_is_bst, right_min, right_max = True, root.val, root.val
+            right_is_bst, right_min, right_max = self.traversal(root.right)
+            left_is_bst, left_min, left_max = self.traversal(root.left)
+            left_is_smaller = left_max < root.val
+            right_is_bigger = root.val < right_min
+
+        is_bst = left_is_bst and right_is_bst and left_is_smaller and right_is_bigger
+        return is_bst, left_min, right_max
 
     def isValidBST(self, root):
         """
@@ -195,3 +295,10 @@ class Solution:
         # this is a valid bst if root.left is a valid bst, root.right is a valid bst, and max of left subtree < this < min of right subtree
         # think of the relationship between this and in order tree traversal
         # how can you return the right most (should be the max) and left most (should be the min) of a subtree
+        if root is None:
+            return True
+        return self.traversal(root)[0]
+
+class Solution:
+    def isValidBST(self, root):
+        # use in order traversal iterative
