0c77a95b181d07e799dcf19c7385a02d8003a218
mtran2011 <tranhaminh@gmail.com>
2018-01-22 22:16:31 -0500

    Update leetcode3.py

--- leetcode3.py
+++ leetcode3.py
@@ -1,3 +1,178 @@
+# 647. Palindromic Substrings
+class Solution:
+    # dp method, O(n^2)
+    def countSubstrings(self, s):
+        n = len(s)
+        if n < 1:
+            return 0
+        # f[start][end] is True if s[start:end+1] is palindrome
+        f = [[False for _ in range(n)] for _ in range(n)]
+        for k in range(n):
+            f[k][k] = True
+            # for convenience later
+            if k+1 < n:
+                f[k+1][k] = True
+        count = n
+        for length in range(1,n):
+            for start in range(n-length):
+                end = start + length
+                if s[start] == s[end]:
+                    f[start][end] = f[start+1][end-1]
+                else:
+                    f[start][end] = False
+                if f[start][end] is True:
+                    count += 1
+        return count
+
+class Solution:
+    # look for the mid point of a palindrome and expand
+    def countSubstrings(self, s):
+        n = len(s)
+        if n < 1:
+            return 0
+        count = n
+        for mid in range(n-1):
+            # case when s[mid] is the only mid point 
+            start, end = mid-1, mid+1
+            while start >= 0 and end < n:
+                if s[start] == s[end]:
+                    count += 1
+                    start -= 1
+                    end += 1
+                else:
+                    break
+            # case when s[mid] and s[mid+1] are two mid points
+            if s[mid] == s[mid+1]:
+                count += 1
+                start, end = mid-1, mid+2
+                while start >= 0 and end < n:
+                    if s[start] == s[end]:
+                        count += 1
+                        start -= 1
+                        end += 1
+                    else:
+                        break
+        return count
+
+# 516. Longest Palindromic Subsequence
+class Solution:
+    def longestPalindromeSubseq(self, s):
+        n = len(s)
+        if n < 1:
+            return 0
+        # define f[i,j] = length of longest palindromic subseq between i, j
+        f = [[0 for _ in range(n)] for _ in range(n)]
+        for k in range(n):
+            f[k][k] = 1
+        for length in range(1,n):
+            for begin in range(n-length):
+                end = begin + length
+                if s[begin] == s[end]:
+                    f[begin][end] = 2 + f[begin+1][end-1]
+                else:
+                    f[begin][end] = max(f[begin+1][end], f[begin][end-1])
+                
+        return f[0][n-1]
+
+# 120. Triangle
+class Solution:
+    def minimumTotal(self, triangle):
+        n = len(triangle)
+        if n < 1:
+            return 0
+        f = [[None for _ in range(len(triangle[i]))] for i in range(n)]
+        # f[x] = min sum of path starting at position x
+        # the bottom of f is the same as bottom of triangle
+        for j in range(len(triangle[-1])):
+            f[-1][j] = triangle[-1][j]
+        # build f from the bottom up
+        for row in range(n-2,-1,-1):
+            for col in range(len(triangle[row])):
+                f[row][col] = triangle[row][col] + min(f[row+1][col], f[row+1][col+1])
+        return f[0][0]
+
+# 139. Word Break
+class Solution:
+    # DP method
+    def wordBreak(self, s, wordDict):
+        n = len(s)
+        wordDict = set(wordDict)
+        # f[i] is True if s[:i] is breakable
+        f = [False for _ in range(n+1)]
+        f[0] = True
+        for i in range(1,n+1):
+            # determine f[i] and already know f[0] to f[i-1]
+            # f[i] is when you add the new element s[i-1]
+            for length in range(1,i+1):
+                start = i-length
+                if s[start:i] in wordDict and f[start] is True:
+                    f[i] = True
+                    break
+        return f[-1]
+
+class Solution:
+    # DFS method
+    def dfs(self, s, wordDict):
+        # recursive base case
+        if s in wordDict:
+            return True        
+        for word in wordDict:
+            k = len(word)
+            if k <= len(s):
+                if s[:k] == word:
+                    if self.dfs(s[k:], wordDict):
+                        # this step is wasteful, for example if all word in dict has k=3
+                        # then repeated check for nothing
+                        return True
+        return False
+
+    def wordBreak(self, s, wordDict):
+        # need to see that if a string u is breakable
+        # then it's equivalent to u being in wordDict
+        wordDict = set(wordDict)
+        return self.dfs(s, wordDict)
+
+# 152. Maximum Product Subarray
+class Solution:
+    def maxProduct(self, nums):
+        # max(k) and min(k) = max and min product of subarray ending at exactly k
+        # as you move forward, to get max(k+1) and min(k+1) you only need the previous max min
+        # so you don't need to store the whole historical max, min
+        n = len(nums)
+        if n < 1:
+            return 0
+        localmax, localmin, globalmax = nums[0], nums[0], nums[0]
+        for i in range(1,n):
+            a, b = localmax * nums[i], localmin * nums[i]
+            localmax = max(a, b, nums[i])
+            localmin = min(a, b, nums[i])
+            globalmax = max(globalmax, localmax)
+        return globalmax
+
+# 213. House Robber II
+class Solution:
+    def rob_straight_row(self, nums, begin, end):
+        f = [0 for _ in range(len(nums))]
+        f[begin] = nums[begin]
+        for k in range(begin+1, end+1):
+            if k-2 >= 0:
+                f[k] = max(f[k-1], f[k-2] + nums[k])
+            else:
+                f[k] = max(f[k-1], nums[k])
+        return f[end]
+
+    def rob(self, nums):
+        n = len(nums)
+        if n < 1:
+            return 0
+        if n < 2:
+            return nums[0]
+        # using two passes
+        x = self.rob_straight_row(nums, 1, n-1)
+        y = self.rob_straight_row(nums, 0, n-2)
+        print('x={}, y={}'.format(x,y))
+        return max(x,y)
+
 # 123. Best Time to Buy and Sell Stock III
 class Solution:
     def profit_ktrades(self, prices, ntrades):
